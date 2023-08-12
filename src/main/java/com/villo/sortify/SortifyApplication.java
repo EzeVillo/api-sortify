f971273ffa0d114aebdc5a0669885a8d250b4dd0
EzeVillo <martinezequielvillordo@gmail.com>
2023-08-11 22:59:49 -0300

    Initialize project

---
+++ src/main/java/com/villo/sortify/SortifyApplication.java
@@ -0,0 +1,13 @@
+package com.villo.sortify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortifyApplication.class, args);
+	}
+
+}
