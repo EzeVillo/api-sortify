7cfc199a4c6b04d78c60daa644f21a29e4ec5b22
EzeVillo <martinezequielvillordo@gmail.com>
2023-08-24 00:30:14 -0300

    contrato Controller

--- src/main/java/com/villo/sortify/controller/PlayListController.java
+++ src/main/java/com/villo/sortify/controller/PlayListController.java
@@ -3,13 +3,16 @@ package com.villo.sortify.controller;
 import com.villo.sortify.dto.response.PlayListResponseDTO;
 import com.villo.sortify.dto.response.TrackPlayListResponseDTO;
 import com.villo.sortify.service.PlayListService;
+import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,6 +30,12 @@ public class PlayListController {
     public ResponseEntity<TrackPlayListResponseDTO> getTracksFromPlayList(
             @RequestHeader("Authorization") final String auth, @PathVariable final String playListId) {
         return new ResponseEntity<>(this.playListService.getTracksFromPlayList(auth, playListId), HttpStatus.OK);
+    }
+
+    @PutMapping(value = "{playListId}/sort")
+    public ResponseEntity<Void> sortTracksFromPlayList(
+            @RequestHeader("Authorization") final String auth, @PathVariable final String playListId, @RequestParam(required = false) final String orderBy) {
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
